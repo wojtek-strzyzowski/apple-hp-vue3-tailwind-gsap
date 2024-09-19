d6d7dcf5fcd7af0cfa8627802c318456a23cf872
Maria Strzyowska <strzyzowski@gmx.de>
2024-09-19 14:15:36 +0200

    finished hero section

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -19,6 +19,12 @@ onMounted(() => {
     delay: 1.5,
   });
 
+  gsap.to('#cta', {
+    opacity: 1,
+    y: -50,
+    delay: 2.2,
+  })
+
   updateVideoSRC();
   window.addEventListener('resize', updateVideoSRC);
 });
@@ -31,7 +37,11 @@ onMounted(() => {
             <div class="md:w-10/12 w-9/12">
                 <video class="pointer-events-none" :src="videoSRC" type="video/mp4" autoplay muted playsinline></video>
             </div>
-        
+        </div>
+
+        <div id="cta" class="flex flex-col items-center opacity-0 translate-y-20">
+          <a href="#highlights" class="btn">Buy</a>
+          <p class="font-normal text-xl">From $199/month or $999</p>
         </div>
     </section>
 
